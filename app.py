cf8b1b375f835fb2f51e27fba497965e2de213d8
Elmira <ibragimovagim@gmail.com>
2019-09-19 20:54:08 +0200

    reset the game

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+from copy import deepcopy
+
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+# the state when the game is reset
+GAME_START_TABLE = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+# stores the current state of the game
 table = [
     [0, 1, 2],
     [3, 4, 5],
@@ -20,3 +30,9 @@ def turn_handler():
     table[x][y] = -1
     return jsonify({'x': x, 'y': y, 'table': str(table)})
 
+
+@app.route('/reset', methods=['POST'])
+def reset_handler():
+    global table
+    table = deepcopy(GAME_START_TABLE)
+    return 'OK'
