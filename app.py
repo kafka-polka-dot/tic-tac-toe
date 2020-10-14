ccf70b3825babb7a13fa7b022aa1d36c27fd44ef
Kirill Khaidukov <kkhaidukov@gmail.com>
2020-10-14 09:15:35 +0200

    distinguish between 2 players

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 import numpy
 
 app = Flask(__name__)
@@ -16,18 +16,39 @@ table = [
     [N, N, N]
 ]
 
+users = []
+
+
+def flatten_table():
+    return tuple(''.join([''.join(e) for e in table]))
+
 
 @app.route('/')
 def index_handler():
-    flat_table = tuple(''.join([''.join(e) for e in table]))
-    return render_template('index.html', cell=flat_table)
+    resp = make_response(render_template('index.html', cell=flatten_table()))
+
+    next_user_id = None
+    if not users:
+        next_user_id = X
+    elif len(users) == 1:
+        next_user_id = O
+    else:
+        next_user_id = None
+
+    cookies = request.cookies.get('user_id')
+    if not cookies and next_user_id:
+        resp.set_cookie('user_id', next_user_id)
+        users.append(next_user_id)
+    return resp
 
 
 @app.route('/turn', methods=['POST'])
 def turn_handler():
+    user = request.cookies.get("user_id")
+
     x = int(request.form['x'])
     y = int(request.form['y'])
-    table[y][x] = X
+    table[y][x] = user
 
     # check if somebody won
     rotated = numpy.rot90(deepcopy(table))
@@ -38,14 +59,14 @@ def turn_handler():
     ]
 
     winner = None
-    for l in check_list:
-        if ''.join(l) == 'XXX':
+    for list_item in check_list:
+        if ''.join(list_item) == 'XXX':
             winner = X
-        elif ''.join(l) == 'OOO':
+        elif ''.join(list_item) == 'OOO':
             winner = O
 
     # FIXME: actual last turn
-    result = {'last_turn': X, 'winner': winner if winner else ''}
+    result = {'last_turn': user, 'winner': winner if winner else ''}
     return jsonify(result)
 
 
